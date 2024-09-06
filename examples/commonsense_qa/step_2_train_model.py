5a03068d981ad8da60a0407bcad14a71a314ff24
Honey田兴 <honeyTian@nxcloud.com>
2024-09-06 15:25:15 +0800

    update

--- examples/commonsense_qa/step_2_train_model.py
+++ examples/commonsense_qa/step_2_train_model.py
@@ -145,8 +145,8 @@ def main():
     ]
 
     data_collator = DataCollatorForCompletionOnlyLM(
-        response_template="assistant",
-        instruction_template="user",
+        response_template="assistant<|end_header_id|>\n",
+        instruction_template="user<|end_header_id|>\n",
         tokenizer=tokenizer
     )
     print(f"data_collator: ")
